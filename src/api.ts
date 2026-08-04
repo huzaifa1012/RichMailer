3ca7cad6fca825222d2ad5395917b9043683fcb8
huzaifa1012 <www.pakteach@gmail.com>
2026-08-05 01:05:01 +0500

    /api added on api util

--- src/api.ts
+++ src/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // baseURL defaults to proxy path during development
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "/api",
+  baseURL: import.meta.env.VITE_API_BASE_URL + "/api" || "/api",
   withCredentials: true, // Enable cookies for session management
 });
 
